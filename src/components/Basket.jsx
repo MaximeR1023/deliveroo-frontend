c6f633108a89190c4e0d27f9c9f6e61c9e3c2fb2
Maxime Riou <maxime.riou27@hotmail.fr>
2025-11-18 23:25:44 +0100

    corrected basket css and functionalities

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -10,59 +10,74 @@ const Basket = ({ basket, setBasket }) => {
   subtotal = subtotal.toLocaleString("fr-FR", { minimumFractionDigits: 2 });
   grandTotal = grandTotal.toLocaleString("fr-FR", { minimumFractionDigits: 2 });
 
+  const isEmpty = basket.length === 0;
+
   return (
     <aside>
       <article className="basket">
-        <button>Valider mon panier</button>
-        {basket.map((element, index) => (
-          <section className="basket-item" key={index}>
-            <div className="item-counter">
-              <button
-                onClick={() => {
-                  const newBasket = [...basket];
-                  newBasket[index].quantity--;
-                  if (newBasket[index].quantity <= 0) {
-                    newBasket.splice(index, 1);
-                  }
-                  setBasket(newBasket);
-                }}
-              >
-                <CiCircleMinus />
-              </button>
-              <p className="item-quantity">{element.quantity}</p>
-              <button
-                onClick={() => {
-                  const newBasket = [...basket];
-                  newBasket[index].quantity++;
-                  setBasket(newBasket);
-                }}
-              >
-                <CiCirclePlus />
-              </button>
-            </div>
-            <p className="item-designation">{element.title}</p>
-            <p className="item-price">
-              {Number(element.price).toLocaleString("fr-FR", {
-                minimumFractionDigits: 2,
-              })}{" "}
-              €
-            </p>
-          </section>
-        ))}
-        <hr />
-        <section className="subtotal">
-          <p>Sous-total</p>
-          <p>{subtotal} €</p>
-        </section>
-        <section className="delivery-fee">
-          <p>Frais de livraison</p>
-          <p>2,50 €</p>
-        </section>
-        <hr />
-        <section className="total">
-          <p>Total</p>
-          <p>{grandTotal} €</p>
-        </section>
+        <button className={isEmpty && "empty-button"}>
+          Valider mon panier
+        </button>
+        {isEmpty ? (
+          <p className="empty-basket-notif">Votre panier est vide</p>
+        ) : (
+          <>
+            {basket.map((element, index) => (
+              <section className="basket-item" key={index}>
+                <div className="basket-item-left">
+                  <div className="item-counter">
+                    <button
+                      onClick={() => {
+                        const newBasket = [...basket];
+                        newBasket[index].quantity--;
+                        if (newBasket[index].quantity <= 0) {
+                          newBasket.splice(index, 1);
+                        }
+                        setBasket(newBasket);
+                      }}
+                    >
+                      <CiCircleMinus />
+                    </button>
+                    <p className="item-quantity">{element.quantity}</p>
+                    <button
+                      onClick={() => {
+                        const newBasket = [...basket];
+                        newBasket[index].quantity++;
+                        setBasket(newBasket);
+                      }}
+                    >
+                      <CiCirclePlus />
+                    </button>
+                  </div>
+                  <p className="item-designation">{element.title}</p>
+                </div>
+                <p className="item-price">
+                  {Number(element.price * element.quantity).toLocaleString(
+                    "fr-FR",
+                    {
+                      minimumFractionDigits: 2,
+                    }
+                  )}{" "}
+                  €
+                </p>
+              </section>
+            ))}
+            <hr />
+            <section className="subtotal">
+              <p>Sous-total</p>
+              <p>{subtotal} €</p>
+            </section>
+            <section className="delivery-fee">
+              <p>Frais de livraison</p>
+              <p>2,50 €</p>
+            </section>
+            <hr />
+            <section className="total">
+              <p>Total</p>
+              <p>{grandTotal} €</p>
+            </section>
+          </>
+        )}
       </article>
     </aside>
   );
